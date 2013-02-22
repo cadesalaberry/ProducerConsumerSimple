40cc7894cd9c4707aaaf6da730fe34095d23eec3
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-02-21 22:30:12 -0500

    Code cleaned.

--- factory.c
+++ factory.c
@@ -1,16 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <bsd/stdlib.h>
-#include <sys/time.h>
 #include "factory.h"
 
 queue q;
-
-int to_consume;
-int to_produce;
-int sync_on = 1;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -27,26 +17,8 @@ int main(int argc, char *argv[]) {
 	// Initiates random seed
 	srand(time(NULL));
 
-	// Defaults units to produce to 50
-	int nb_prod = 50;
-
 	// Deals with extra arguments
-	if(argc > 1) {
-
-		if(!strcmp(argv[1], "-async")) {
-			// Disables sync mode
-			sync_on = 0;
-		} else {
-			nb_prod = atoi(argv[1]);
-		}
-
-		if(argc == 3) {
-			nb_prod = atoi(argv[2]);
-		}
-
-	} else {
-		return 1;
-	}
+	read_args(argc, argv);
 
 	// Determines what to consume and what to produce
 	to_consume = NB_PROD_THREAD / NB_CONS_THREAD * nb_prod;
@@ -61,74 +33,81 @@ int main(int argc, char *argv[]) {
 	for(id = 0; id < NB_PROD_THREAD; id++) {
 		
 		// Creates the producer threads
-		if(pthread_create(&prod_thread[id], NULL, producer, NULL)) {
-			
+		if(pthread_create(&prod_thread[id], NULL, producer, NULL)) {		
 			printf("Error creating producer thread #%d\n", id);
 			return 1;
 		}
 
 		// Joins the producer threads
-		if(pthread_join(prod_thread[id], NULL))
-		{
+		if(pthread_join(prod_thread[id], NULL)) {
 			printf("Error joining producer thread #%d\n", id);
 			return 1;
 		}
 	}
-
 	
-	for(id = 0; id < NB_PROD_THREAD; id++) {
+	for(id = 0; id < NB_CONS_THREAD; id++) {
 		
 		// Creates the consumer threads
 		if(pthread_create(&cons_thread[id], NULL, consumer, NULL)) {
-			
 			printf("Error creating customer thread\n");
 			return 1;
 		}
-	}
 
-	printf("Produced\t: %d\n", q.end);
-	printf("Consumed\t: %d\n", q.start);
+		// Joins the consumer threads
+		if(pthread_join(cons_thread[id], NULL)){
+			printf("Error joining consumer thread #%d\n", id);
+			return 1;
+		}
+	}
 	
 	// Ends Timer
 	gettimeofday(&end, NULL);
-	printf("Time\t\t: %ld.", end.tv_sec - start.tv_sec);
-	printf("%ld\n", end.tv_usec - start.tv_usec);
-	exit(0);
+	long long t_elapsed = (end.tv_sec - start.tv_sec) * 1000000LL;
+	t_elapsed += end.tv_usec-start.tv_usec;
+
+	if (profile) {
+
+		printf("%llu\n", t_elapsed);
+
+	} else {
+	
+		printf("Produced\t: %llu\n", q.end);
+		printf("Consumed\t: %llu\n", q.start);
+		printf("Time\t\t: %llu\n", t_elapsed);
+	}
+
 	return 0;
 }
 
 
 static void * producer() {
 
-	//printf("Producing:%d\n", to_produce);
-
 	int i;
 	for(i = 0; i < to_produce; i++) {
-		
-		int item = rand() % 6969;
 		
 		if(sync_on) {
 			pthread_mutex_lock(&mutex);
 		}
 
-		q.data[q.end] = item;
+		q.data[q.end] = rand() % 6969;
 		
 		//PRINT_PROD
-
 		q.end++;
 
 		if(sync_on) {
-			pthread_cond_signal(&cond);
 			pthread_mutex_unlock(&mutex);
+			pthread_cond_signal(&cond);
 		}
 	}
 	return NULL;
 }
 
-static void * consumer(int * unit) {
+static void * consumer() {
+	
+	// While there is still something to consume
+	int i;
+	for(i = 0; i < to_consume; i++) {
 
-
-	while(q.start < to_consume) {
 		if(sync_on){
 
 			pthread_mutex_lock(&mutex);
@@ -136,19 +115,17 @@ static void * consumer(int * unit) {
 			if(q.end == q.start) {
 				pthread_cond_wait(&cond, &mutex);
 			}
-
 		} else {
 			while(q.end == q.start);
 		}
 
-		PRINT_CONS
-
+		//PRINT_CONS
+		//q.data[q.start] = 0;
 		q.start++;
 		
 		if(sync_on) {
 			pthread_mutex_unlock(&mutex);
 		}
 	}
-	
 	return NULL;
 }
