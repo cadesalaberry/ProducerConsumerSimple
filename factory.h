7dc276a3a036bb50469fcc30bfa5ad5d2d26173d
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-02-21 20:04:16 -0500

    Add: debug line for cons.

--- factory.h
+++ factory.h
@@ -1,7 +1,8 @@
 #define NB_PROD_THREAD 10
 #define NB_CONS_THREAD 1
 
-#define PRINT_PROD //if (!(q.end % 100000)) {printf("@%i: %i produced\n", q.end, item);}
+#define PRINT_PROD if (!(q.end % 100000)) {printf("@%i: %i produced\n", q.end, item);}
+#define PRINT_CONS if (!(q.start % 10000)) {printf("@%i: %i consumed\n", q.start, q.data[q.start]);}
 
 typedef struct {
 	
