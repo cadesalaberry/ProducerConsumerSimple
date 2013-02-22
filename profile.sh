4c8c3fc19f348f9d358b226f7ddc1241c44b542a
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-02-21 22:32:42 -0500

    Add: Profiler script.

---
+++ profile.sh
@@ -0,0 +1,36 @@
+PROG=factory
+SYNC=report_sync.csv
+ASYNC=report_async.csv
+
+if [ -e ./*.o ]
+	then make clean > /dev/null
+fi
+make > /dev/null
+if [ -e ./$PROG ]
+	then
+	
+	echo -n "Logging data for Synchronous... "
+	echo "product, time" > $SYNC
+	
+	for i in {0..1000000..1000}
+	do
+		TIME=$(./$PROG -profile -produce $i)
+		echo $i, $TIME >> $SYNC
+	done
+	echo DONE
+	
+	
+	echo -n "Logging data for Asynchronous... "
+	echo "product, time" > $ASYNC
+	
+	for i in {0..1000000..1000}
+	do
+		TIME=$(./$PROG -async -profile -produce $i)
+		echo $i, $TIME >> $ASYNC
+	done
+	echo DONE
+	
+fi
+if [ -e ./*.o ]
+	then make clean > /dev/null
+fi
