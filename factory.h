f5001523ba3f508ecfeecd98a638f96d5d3f3818
Charles Antoine DE SALABERRY <cadesalaberry@yahoo.com>
2013-02-21 19:54:48 -0500

    Mod: malloc used for queue\nAdd: debug line for prod.

--- factory.h
+++ factory.h
@@ -1,16 +1,18 @@
 #define NB_PROD_THREAD 10
 #define NB_CONS_THREAD 1
-#define MAX_QUEUE 9999999
+
+#define PRINT_PROD //if (!(q.end % 100000)) {printf("@%i: %i produced\n", q.end, item);}
 
 typedef struct {
 	
-	int data[MAX_QUEUE];
+	int *data;
 	int start;
 	int end;
 
 } queue;
 
-void init_queue(queue * q) {
+void init_queue(queue * q, int size) {
+	q->data = malloc(size * sizeof(int));
 	q->start = 0;
 	q->end = 0;
 }
